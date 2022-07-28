d7f1b9249f5c06639e525d896f49fd6e0eb1170e
Redwan-anik <redwan229900@gmail.com>
2022-07-28 10:54:05 +0600

    page view worked properly

--- lib/app/modules/workPage/views/work_page_view.dart
+++ lib/app/modules/workPage/views/work_page_view.dart
@@ -21,7 +21,12 @@ class WorkPageView extends GetView<WorkPageController> {
           Container(color: Colors.blue,child: Obx(()=>GestureDetector(onTap:(){
             controller.pageController.nextPage(duration: Duration(seconds: 1), curve: Curves.ease);
           },child: Text('${controller.car.value}')),)),
-          Container(color: Colors.orange,child: Obx(()=>Text('${controller.car.value}'),)),
+          Container(color: Colors.orange,child: Obx(()=>GestureDetector(
+              onTap: (){
+                controller.pageController.nextPage(duration: Duration(seconds: 1), curve: Curves.ease);
+
+              },
+              child: Text('${controller.car.value}')),)),
 
           Container(color: Colors.green,child: Obx(()=>Text('${controller.car.value}'),)),
 
